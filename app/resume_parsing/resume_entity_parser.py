b2cb26e2adb708b4bff32eca8639aa00933c8c80
Mtaylert <mtaylert.scriptify@gmail.com>
2023-01-31 08:52:45 -0500

    entity labels start

--- app/resume_parsing/resume_entity_parser.py
+++ app/resume_parsing/resume_entity_parser.py
@@ -1,7 +1,9 @@
 from app.resume_parsing.text_extration import ResumePageExtraction
 import os
 import re
+import spacy
 
+nlp = spacy.load('en_core_web_sm')
 
 def email_extract(line):
     match = re.search(r"[\w.+-]+@[\w-]+\.[\w.-]+", line)
@@ -18,6 +20,15 @@ def phone_number_extract(line):
     return match
 
 
+def get_name(line):
+    doc = nlp(line)
+    name = None
+    for ent in doc.ents:
+        if ent.label_ == "PERSON":
+            return ent.text
+    return name
+
+
 def section_one_parse(filepath):
     resume_parser = {}
     for file in os.listdir(filepath):
@@ -29,9 +40,17 @@ def section_one_parse(filepath):
                 text_line = " ".join(parsed_pdf_page[page]["text"][line])
                 email = email_extract(text_line)
                 phone = phone_number_extract(text_line)
+                name = get_name(text_line)
                 if email:
                     first_section["email"] = {"text": email, "confidence": 1}
                 if phone:
                     first_section["phone"] = {"text": phone, "confidence": 1}
+                if name:
+                    first_section["name"] = {"text": name, "confidence": 1}
                 resume_parser[file] = first_section
     return resume_parser
+
+
+
+parsed_resumes = section_one_parse(filepath="/Users/home/workplace/Scriptify/test_data/test_resumes/")
+print(parsed_resumes)
--- devtools/annotation.py
+++ devtools/annotation.py
@@ -1,24 +1,48 @@
 from app.resume_parsing.text_extration import ResumePageExtraction
 import os
+import pandas as pd
+import json
 
-filpath = "/Users/home/workplace/Scriptify/test_data/test_resumes/"
-resume_parser = {}
-for file in os.listdir(filpath):
-    first_section = {}
-    full_path = filpath + file
-    parsed_pdf_page = ResumePageExtraction().build_page(pdf_file=full_path)
-    for page in parsed_pdf_page:
-        cutoff = 5
-        page_num = 1
-        if page == page_num:
-            for line in parsed_pdf_page[page]["text"]:
-                if line <= cutoff:
-                    text_line = parsed_pdf_page[page]["text"][line]
-                    for token in text_line:
-                        print(token)
-                        my_input = ()
-                        if my_input == "n":
-                            first_section
-            print()
+def parse():
+    filpath = "/Users/home/workplace/Scriptify/test_data/test_resumes/"
+    resume_parser = []
+    for file in os.listdir(filpath):
+        first_section = {}
+        full_path = filpath + file
+        parsed_pdf_page = ResumePageExtraction().build_page(pdf_file=full_path)
+        for page in parsed_pdf_page:
+            print("*********** NEW RESUME ***********")
+            cutoff = 5
+            page_num = 1
+            if page == page_num:
+                for line in parsed_pdf_page[page]["text"]:
+                    if line <= cutoff:
+                        annotation = {"text":[], "labels":[]}
+                        text_line = parsed_pdf_page[page]["text"][line]
+                        for token in text_line:
+                            annotation['text'].append(token)
+                            annotation['labels'].append('o')
+                        resume_parser.append(annotation)
+    with open('/Users/home/workplace/Scriptify/test_data/entity_annotation/entity_labeling.json', 'w') as file:
+        file.write(json.dumps(resume_parser, indent=4))
+
+def write_ton_jso():
+    with open('entity_labeling_2.json', 'w') as file:
+        for i in range(3):
+            for i in df['Resume'].iloc[i][:200].split('\\n'):
+                annotation = {"text":[], "labels":[]}
+                for word in i.split(' '):
+                    print(word)
+                    my_input = input()
+                    if my_input == "b":
+                        annotation['text'].append(word)
+                        annotation['labels'].append(my_input)
+                    else:
+                        annotation['text'].append(word)
+                        annotation['labels'].append('o')
+                file.write(json.dumps(annotation))
+                file.write('\n')
+        file.close()
 
 
+parse()
